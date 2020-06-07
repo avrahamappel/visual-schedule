11663c05ba70dfbdd7fe3f46f06b8efa7ef330c3
avrahamappel <aappel248@gmail.com>
2020-06-07 18:17:44 -0400

    wip calculating pie chart values

--- app.py
+++ app.py
@@ -14,3 +14,49 @@ def home():
     schedule = get_schedule_from_link(calendar_link)
 
     return render_template('home.html', schedule=schedule)
+
+
+@app.route('/pie')
+def pie():
+    schedule_items = [
+        # (title, start_time, end_time, color)
+        ('Sleep', 4, 5, 'cyan'),
+        ('Sleep', 5, 6, 'pink'),
+        ('Sleep', 6, 7, 'yellow'),
+        ('Sleep', 7, 8, 'red'),
+        ('Sleep', 8, 9, 'blue'),
+        ('Sleep', 9, 10, 'green'),
+        ('Shiur', 10, 11, 'blue'),
+        ('English', 12, 13, 'red'),
+    ]
+
+    mapped = map(schedule_to_stroke, schedule_items)
+
+    return render_template('pie.html', schedule_items=mapped)
+
+
+def schedule_to_stroke(schedule):
+    # start end -> stroke-dasharray
+    # before% length% after%
+    # each hour is (100/24)% of the day
+    # offset is negative, and add +25
+    # length is (end - start)*(hour formula)
+    # after is 100 - (before + length)
+
+    title, start, end, color = schedule
+
+    before = -hour_to_percent(start) + 25
+    length = hour_to_percent(end - start)
+    after = 100 - (before + length)
+
+    return (
+        title,
+        before,
+        length,
+        after,
+        color
+    )
+
+
+def hour_to_percent(val):
+    return val * (100 / 24)
